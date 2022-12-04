166e99d1aa6f8ed5a5b7fa46fe56496e2eba457d
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-12-04 23:10:57 +0530

    Square root of a number
    https://practice.geeksforgeeks.org/problems/square-root/0

---
+++ Medium/Square root of a number.cpp
@@ -0,0 +1,21 @@
+/*
+Given an integer x, find the square root of x. If x is not a perfect square, then return floor(√x).*/
+APPROACH -1
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        return sqrt(x);
+    }
+};
+
+
+
+// class Solution{
+//   public:
+//     long long int floorSqrt(long long int x) 
+//     {
+//           long long a= sqrt(x);
+//           return a;
+//     }
+// };
